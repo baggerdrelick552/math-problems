785feaf54bfea2aeffc5e1887ee698dbc9c95c97
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-03-24 18:43:12 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,14 @@
+<?php
+// This is a test PHP script
+// Please make sure to replace the 'code' and 'result' variables with actual values when running
+
+function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+}
+
+$num1 = 5;
+$num2 = 3;
+
+$result = addNumbers($num1, $num2);
+echo "The sum of $num1 and $num2 is: $result";
+?>
