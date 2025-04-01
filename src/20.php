df110ce36603a05cbb66b3a4f330a01abfa2d736
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-01 14:24:17 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,11 @@
+<?php
+// Example PHP code for solving basic arithmetic equations
+
+// Add 5 and 3
+$sum = 5 + 3;
+echo "The sum is: $sum";
+
+// Subtract 2 from 10
+$difference = 10 - 2;
+echo "The difference is: $difference";
+?>
