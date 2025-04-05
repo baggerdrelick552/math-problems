c7761b0cd1555aea1a40517a2c151b228e9208d3
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 13:32:44 +0000

    Add 106.php

---
+++ src/106.php
@@ -0,0 +1,13 @@
+<?php
+// Define variables
+$test1 = 5; // Example value
+$test2 = "Hello"; // Example value
+
+// Generate random numbers
+$result1 = rand($test1, $test2);
+$result2 = rand($test1, $test2);
+
+// Output the results
+echo "Random number 1: $result1\n";
+echo "Random number 2: $result2\n";
+?>
