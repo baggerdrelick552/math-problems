04d3c6122b7fa51af05eca627380554c6449fa75
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 23:38:50 +0000

    Add 232.php

---
+++ src/232.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the MathProblems package.
+ *
+ * For full license and copyright information,
+ * please refer to the LICENSE.md file at the root directory of this project.
+ */
+
+// Example of a PHP function that calculates the sum of two numbers
+
+function calculateSum($number1, $number2) {
+    return $number1 + $number2;
+}
+
+// Example usage:
+$sum = calculateSum(5, 3);
+echo "The sum is: $sum";
+?>
