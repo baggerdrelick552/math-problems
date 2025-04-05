5769be254dd4711f8bea20fa6474d12ce8ca8d47
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 13:44:58 +0000

    Add 108.php

---
+++ src/108.php
@@ -0,0 +1,10 @@
+<?php
+// Example PHP code for solving math problems from the SPC Math Curriculum
+
+# Define variables and constants
+$variables = array(5, 3, 2, 8); // For example values
+$constant = 7; // A constant value
+
+# Perform calculations
+$result = $variables[0] + $variables[1] * $constant - $variables[2];
+echo "The result is: $result";
