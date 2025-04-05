a3df8253f9452244b55790c38e0da23bf65eefcc
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 18:26:32 +0000

    Add 167.php

---
+++ src/167.php
@@ -0,0 +1,12 @@
+<?php
+// This is a sample PHP code demonstrating some mathematical operations.
+// Please modify and adapt as needed.
+
+$sum = 5 + 3;
+$floor_division = 10 / 2;
+
+echo "The sum of 5 and 3 is: $sum";
+echo "\n";
+
+echo "The floor division of 10 by 2 is: $floor_division";
+?>
