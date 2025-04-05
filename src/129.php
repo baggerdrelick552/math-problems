eff6a6dc745fc75316d5fced31dd7405e5eaeb86
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 15:25:18 +0000

    Add 129.php

---
+++ src/129.php
@@ -0,0 +1,10 @@
+<?php
+$a = 5;
+$b = 10;
+$c = 2;
+
+echo "The sum of $a and $b is $c.\n";
+echo "The product of $a and $b is $a * $b.\n";
+echo "The difference between $a and $b is $a - $b.\n";
+echo "The quotient of $a divided by $b is $a / $b.\n";
+?>
