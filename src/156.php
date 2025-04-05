88fea53f105609eba063f01d57d8eb04ea6cd8dd
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 17:36:15 +0000

    Add 156.php

---
+++ src/156.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of MathProblems Solutions.
+ *
+ * MathProblems Solutions is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MathProblems Solutions is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MathProblems Solutions. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Example PHP code for solving math problems from the school's curriculum
+// Replace this with actual math equations from your textbook or course material
+
+print "Solution 1:\n";
+echo "The solution to the equation \(a^2 - b^2 = c\) is: $b";
+
+print "\n\nExample 2 Solution:\n";
+$a = 5; $b = 3; $c = 7;
+echo "For a=$a, b=$b and c=$c, the solution of the equation \(a^2 - b^2 = c\) is: $c";
+
+// Continue adding more examples as needed
+?>
