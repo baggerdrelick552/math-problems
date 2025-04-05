12c278c327eafe5129bdaaa30fcba8eb793c893b
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 08:18:19 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$x = 5;
+$y = 10;
+
+// Check if $x is greater than $y
+if ($x > $y) {
+    echo "The value of x is greater than y.";
+} else {
+    echo "The value of x is not greater than y.";
+}
+?>
