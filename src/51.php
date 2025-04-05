b33df2058f8fc326723614b703e1d8e148a6a8dc
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 09:06:16 +0000

    Add 51.php

---
+++ src/51.php
@@ -0,0 +1,11 @@
+<?php
+// Example PHP code related to mathematics and algebra
+
+function sumOfSquares($a, $b) {
+    return $a * $a + $b * $b;
+}
+
+function differenceOfSquares($a, $b) {
+    return $a * $a - $b * $b;
+}
+?>
