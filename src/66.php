94a51c3c178df3cc65110dc7893729a3e57fa60a
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-05 10:19:39 +0000

    Add 66.php

---
+++ src/66.php
@@ -0,0 +1,8 @@
+<?php
+// Assume $problem is an array of questions and their corresponding solutions
+
+foreach ($problem as $question => $solution) {
+    echo "Question: $question<br>";
+    echo "Solution: $solution<br><br>";
+}
+?>
