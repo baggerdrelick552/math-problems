695fd7031515e3b4f14ac98cf48fd1fb6c6b681b
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-06 06:27:18 +0000

    Add 309.php

---
+++ src/309.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "Value 1";
+$var2 = "Value 2";
+
+// Check if variables are defined
+if (isset($var1) && isset($var2)) {
+    echo "Variables $var1 and $var2 are defined.";
+} else {
+    echo "One or more of the variables is undefined.";
+}
+?>
