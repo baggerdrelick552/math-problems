bf3491da80d0270430bf25fa3884bdc108d52bf5
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-06 00:40:32 +0000

    Add 244.php

---
+++ src/244.php
@@ -0,0 +1,11 @@
+<?php
+// Define variables and constants
+$x = 5; // Example value
+
+// Check if x is less than or equal to 10
+if ($x <= 10) {
+    echo "The value of x is within the range [5, 10].";
+} else {
+    echo "The value of x is outside the range [5, 10].";
+}
+?>
