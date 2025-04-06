d1e3abfbbf36a4258a8643bd8c7eac02d25f2b6d
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-06 05:40:49 +0000

    Add 300.php

---
+++ src/300.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants
+$var1 = "value1";
+$var2 = 42;
+
+// Check if variable $var2 is greater than or equal to 0
+if ($var2 >= 0) {
+    echo "The value of $var2 is greater than or equal to 0.";
+} else {
+    echo "The value of $var2 is not greater than or equal to 0.";
+}
+?>
