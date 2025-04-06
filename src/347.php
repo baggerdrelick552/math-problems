cd2eff332d76cd201cdb7e6de52de01b26f4515e
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-06 12:39:21 +0000

    Add 347.php

---
+++ src/347.php
@@ -0,0 +1,18 @@
+<?php
+// Your mathematical problem here...
+
+// Example: A = b^2 + c * d
+
+$b = 5;
+$c = 3;
+$d = 7;
+
+$a = $b**2 + $c*$d;
+echo "The value of a is: " . $a;
+
+// Another example:
+$base = 10;
+ exponent = 2;
+ result = pow($base, $exponent);
+echo "The result of base raised to the power of $exponent is: " . $result;
+?>
