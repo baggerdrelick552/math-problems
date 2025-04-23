8e7ec8b50659fb0bd295884626d3f35c2c293dc9
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-23 10:18:45 +0000

    Add 372.php

---
+++ src/372.php
@@ -0,0 +1,21 @@
+<?php
+// Example PHP code related to the Math Problems series in math-problems (Solutions to math problems from your school's curriculum)
+// This is just an example and does not represent actual math problems or solutions.
+
+// Define some variables for demonstration
+$test1 = 5;
+$test2 = 3.9;
+
+// Perform arithmetic operations
+$result1 = $test1 + $test2; // Addition
+$result2 = $test1 - $test2; // Subtraction
+$result3 = $test1 * $test2; // Multiplication
+$result4 = $test1 / $test2; // Division
+
+// Print the results
+echo "Addition: $result1\n";
+echo "Subtraction: $result2\n";
+echo "Multiplication: $result3\n";
+echo "Division: $result4\n";
+
+?>
