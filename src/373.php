735d6170d40899de55fbf0e397d4a6f8109389e1
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-04-24 02:54:15 +0000

    Add 373.php

---
+++ src/373.php
@@ -0,0 +1,10 @@
+<?php
+// Define the function name and parameters
+function multiply($a, $b) {
+    return $a * $b;
+}
+
+// Call the function with two numbers
+$result = multiply(5, 3);
+echo "The result of 5 multiplied by 3 is: " . $result;
+?>
