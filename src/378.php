b6c093e9fa68635f4ee2b9dd404b95483c7a9bfa
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-05-18 14:18:54 +0000

    Add 378.php

---
+++ src/378.php
@@ -0,0 +1,11 @@
+<?php
+$questions = [
+    "What is 3 + 5?",
+    "What is 10 - 2?",
+    "What is 7 * 8?",
+    "What is 9 / 4?",
+    "What is 6 + 9?",
+    "What is 2 ^ 3?",
+    "What is 5^2?",
+    "What is the result of (4 + 2) * 5?"
+];
