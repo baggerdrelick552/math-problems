5fc3a73806b98146616f6190fa1df5c324654a66
baggerdrelick552 <baggerdrelick552@gmail.com>
2025-05-23 05:55:32 +0000

    Add 380.php

---
+++ src/380.php
@@ -0,0 +1,6 @@
+<?php
+  $result = array(
+    "math" => "The formula for solving a quadratic equation ax^2 + bx + c = 0 is x = [-b ± sqrt(b^2 - 4ac)] / (2a)",
+    "geometry" => "In Euclidean geometry, the sum of angles in any triangle is 180 degrees."
+  );
+?>
